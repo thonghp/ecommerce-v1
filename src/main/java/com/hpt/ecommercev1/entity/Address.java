ac29ae0b75951b122901b67a6b7fd7392a7759ee
thonghp <thonghoangpham123@gmail.com>
2023-02-02 18:33:15 +0700

    #3 - add user, role and address entity

---
+++ src/main/java/com/hpt/ecommercev1/entity/Address.java
@@ -0,0 +1,18 @@
+package com.hpt.ecommercev1.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Address {
+    private Integer id;
+    private String street;
+    private String ward;
+    private String district;
+    private String city;
+    private User user;
+}
+
